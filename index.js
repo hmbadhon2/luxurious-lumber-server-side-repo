79a2953ade6e177002ee7a7b8adbf846ac17ffc4
hmbadhon2 <hmmizanurrahmanbadhon@gmail.com>
2022-11-26 08:24:25 +0600

    product post and get complete

--- index.js
+++ index.js
@@ -10,20 +10,47 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.DB_PASSWORD}@cluster0.hvwcwlz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log(uri)
 
 async function run(){
+
     try{
+        const CategoriesCollection = client.db("luxuriousLumber").collection("Categories");
+        const productsCollection = client.db("luxuriousLumber").collection("products");
+
         const usersCollection = client.db("luxuriousLumber").collection("users");
 
+        app.get('/categories',async(req,res)=>{
+            const query ={};
+            const categories = await CategoriesCollection.find(query).toArray();
+            res.send(categories)
+        })
+        app.get('/categories/:id', async(req, res)=>{
+            const id = req.params.id;
+            const filter= {_id:ObjectId(id)}
+            const result = await CategoriesCollection.findOne(filter);
+            res.send(result)
+        })
         app.get('/users', async(req,res)=>{
             const query = {}
             const users = await usersCollection.find(query).toArray();
             res.send(users)
         })
+
+        app.get('/products', async(req,res)=>{
+            const query ={};
+            const products = await productsCollection.find(query).toArray();
+            res.send(products)
+        })
+
+        app.post('/products', async(req,res)=>{
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.send(result)
+        } )
         app.post('/users', async(req,res)=>{
             const user = req.body;
             const result = await usersCollection.insertOne(user);
